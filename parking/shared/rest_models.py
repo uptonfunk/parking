6b7e307c349572d115b94226db9edb00cbd15c26
Harvey Hunt <harveyhuntnexus@gmail.com>
2018-05-22 15:02:39 +0100

    serialise: Rename REST models and remove error field from response
    Signed-off-by: Harvey Hunt <harveyhuntnexus@gmail.com>

--- parking/shared/rest_models.py
+++ parking/shared/rest_models.py
@@ -15,14 +15,13 @@ class ParkingLot:
 @attr.s
 class ParkingLotCreationResponse:
     id: int = attr.ib(validator=[attr.validators.instance_of(int), validate_non_neg], default=0)
-    errors: list = attr.ib(validator=attr.validators.instance_of(list), factory=list)
 
 
 @attr.s
-class SpaceAvailableMessage:
+class ParkingLotAvailableMessage:
     available: int = attr.ib(validator=[attr.validators.instance_of(int), validate_non_neg])
 
 
 @attr.s
-class SpacePriceMessage:
+class ParkingLotPriceMessage:
     price: float = attr.ib(validator=[attr.validators.instance_of(float), validate_non_neg])
