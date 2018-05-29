099e794a84d194f11795666f82688ad99bd56012
manfredlift <liiv.manfred@gmail.com>
2018-05-29 16:15:00 +0100

    ws: Finished unit tests

--- tests/test_user_server.py
+++ tests/test_user_server.py
@@ -12,7 +12,9 @@ from parking.backend.user_server.session import UserSessions
 from parking.backend.user_server.wsserver import UserWSHandler
 from parking.shared.location import Location
 from parking.shared.util import serialize_model
-from parking.shared.ws_models import deserialize_ws_message, WebSocketErrorType, LocationUpdateMessage
+from parking.shared.ws_models import deserialize_ws_message, WebSocketErrorType, LocationUpdateMessage, \
+    ParkingRequestMessage, ParkingAcceptanceMessage, ConfirmationMessage, ParkingRejectionMessage, \
+    ParkingCancellationMessage, ParkingLotAllocation, ParkingAllocationMessage
 
 
 async def return_async_value(val):
@@ -122,3 +124,99 @@ async def test_location_update_message(ws_url, user_id, mocker, usessions):
     await conn.write_message(serialize_model(LocationUpdateMessage(location)))
     await sleep(0.01)
     usessions.update_user_location.assert_called_once_with(user_id, location)
+
+
+@pytest.mark.gen_test(run_sync=False)
+async def test_parking_request_message(ws_url, user_id, mocker, engine):
+    user_location = Location(0.0, 1.0)
+    lot_location = Location(0.001, 1.0)
+    lot_allocation = ParkingLotAllocation(100, "test_lot", 0, lot_location, 1, distance=50.0, availability=20)
+    parking_request = ParkingRequestMessage(user_location)
+
+    mocker.patch.object(engine, 'handle_allocation_request')
+    engine.handle_allocation_request.return_value = return_async_value(lot_allocation)
+
+    conn: WebSocketClientConnection = await tornado.websocket.websocket_connect(ws_url + user_id)
+    await conn.write_message(serialize_model(parking_request))
+    msg = deserialize_ws_message(await conn.read_message())
+
+    engine.handle_allocation_request.assert_called_once_with(user_id, parking_request)
+    assert isinstance(msg, ParkingAllocationMessage)
+    assert msg.lot == lot_allocation
+
+
+@pytest.mark.gen_test(run_sync=False)
+async def test_parking_request_message_no_parking_lot(ws_url, user_id, mocker, engine):
+    user_location = Location(0.0, 1.0)
+    parking_request = ParkingRequestMessage(user_location)
+
+    mocker.patch.object(engine, 'handle_allocation_request')
+    engine.handle_allocation_request.return_value = return_async_value(None)
+
+    conn: WebSocketClientConnection = await tornado.websocket.websocket_connect(ws_url + user_id)
+    await conn.write_message(serialize_model(parking_request))
+    msg = deserialize_ws_message(await conn.read_message())
+
+    engine.handle_allocation_request.assert_called_once_with(user_id, parking_request)
+    assert WebSocketErrorType(msg.error) == WebSocketErrorType.NO_AVAILABLE_PARKING_LOT
+
+
+@pytest.mark.gen_test(run_sync=False)
+async def test_parking_acceptance_message_happy_case(ws_url, user_id, mocker, dbaccess, engine):
+    lot_id = 7
+
+    mocker.patch.object(dbaccess, "allocate_parking_lot")
+    dbaccess.allocate_parking_lot.return_value = return_async_value(True)
+    mocker.spy(engine, "commit_allocation")
+
+    conn: WebSocketClientConnection = await tornado.websocket.websocket_connect(ws_url + user_id)
+
+    await conn.write_message(serialize_model(ParkingAcceptanceMessage(lot_id)))
+    msg = deserialize_ws_message(await conn.read_message())
+
+    engine.commit_allocation.assert_called_once_with(user_id, lot_id)
+    assert isinstance(msg, ConfirmationMessage)
+
+
+@pytest.mark.gen_test(run_sync=False)
+async def test_parking_acceptance_message_failed_commit(ws_url, user_id, mocker, dbaccess, engine):
+    lot_id = 7
+
+    mocker.patch.object(dbaccess, "allocate_parking_lot")
+    dbaccess.allocate_parking_lot.return_value = return_async_value(False)
+    mocker.spy(engine, "commit_allocation")
+
+    conn: WebSocketClientConnection = await tornado.websocket.websocket_connect(ws_url + user_id)
+
+    await conn.write_message(serialize_model(ParkingAcceptanceMessage(lot_id)))
+    msg = deserialize_ws_message(await conn.read_message())
+
+    engine.commit_allocation.assert_called_once_with(user_id, lot_id)
+    assert WebSocketErrorType(msg.error) == WebSocketErrorType.ALLOCATION_COMMIT_FAIL
+
+
+@pytest.mark.gen_test(run_sync=False)
+async def test_parking_rejection_message(ws_url, user_id, mocker, usessions):
+    lot_id = 7
+    mocker.spy(usessions, "add_user_rejection")
+
+    conn: WebSocketClientConnection = await tornado.websocket.websocket_connect(ws_url + user_id)
+
+    await conn.write_message(serialize_model(ParkingRejectionMessage(lot_id)))
+    msg = deserialize_ws_message(await conn.read_message())
+
+    usessions.add_user_rejection.assert_called_once_with(user_id, lot_id)
+    assert isinstance(msg, ConfirmationMessage)
+
+
+@pytest.mark.gen_test(run_sync=False)
+async def test_parking_cancellation_message(ws_url, user_id, dbaccess):
+    lot_id = 7
+    dbaccess.delete_allocation.return_value = return_async_value(None)
+
+    conn: WebSocketClientConnection = await tornado.websocket.websocket_connect(ws_url + user_id)
+    await conn.write_message(serialize_model(ParkingCancellationMessage(lot_id)))
+
+    assert await conn.read_message() is None  # assert connection was closed by server
+    await sleep(0.01)
+    dbaccess.delete_allocation.assert_called_once_with(user_id)
