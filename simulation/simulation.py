b1e267db3b59aeedca989220ccbc111f80cbc3a3
uptonfunk <johnupton64@hotmail.co.uk>
2018-06-01 01:52:31 +0100

    refactor and remove error in lat long conversion

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -2,7 +2,6 @@ import asyncio
 import random
 import time
 import tkinter as tk
-import math
 from tornado import httpclient
 from concurrent import futures
 import logging
@@ -60,12 +59,12 @@ class SimManager:
             name += 1
 
         for i in range(self.no_cars):
-            start_time = 5
+            start_time = 1
             p = self.point_to_location(self.random_lot.randint(0, width), self.random_lot.randint(0, height))
             coro = car_routine(start_time, p, self)
             self.car_tasks.append(asyncio.ensure_future(coro))
 
-        rogue_start = 3
+        rogue_start = 1
         for i in range(self.no_rogues):
             if random.randint(1, 2) == 1:
                 locx = random.choice([0, width])
@@ -83,15 +82,15 @@ class SimManager:
     def point_to_location(self, x: float, y: float) -> wsmodels.Location:
         """Assuming (0, 0) x/y maps to Location(0, 0), compute the Location for an arbitrary x, y point
         """
-        return wsmodels.Location(x / SCALE, y / SCALE)
+        return wsmodels.Location(x / (SCALE), y / (SCALE))
 
     def loc_to_point(self, loc: wsmodels.Location):
         """Assuming (0, 0) x/y maps to Location(0, 0), compute the Location for an arbitrary x, y point
         """
-        return (loc.longitude * SCALE, loc.latitude * SCALE)
+        return (loc.latitude * (SCALE), loc.longitude * (SCALE))
 
     async def run_tk(self, root, interval):
-        w = tk.Canvas(root, width=self.width*2, height=self.height*3)
+        w = tk.Canvas(root, width=self.width, height=self.height)
         w.pack()
         try:
             while not self.stop_flag:
@@ -99,7 +98,7 @@ class SimManager:
                 now = time.time()
                 for simlot in self.lots:
                     x, y = self.loc_to_point(simlot.lot.location)
-                    w.create_rectangle(x, y, x + 20, y + 20, width=0, fill="green")
+                    w.create_rectangle(x, y, x + (simlot.available*4), y + (simlot.available*4), width=0, fill="green")
 
                 for car in self.cars:
                     if car.drawing:
@@ -165,8 +164,6 @@ class RogueCar:
         self.startY = loc.longitude
         self.destX = dest.latitude
         self.destY = dest.longitude
-        self.parkX = 0
-        self.parkY = 0
         self.waypoints = []
         self.drawing = True
         self.starttime = starttime
@@ -184,17 +181,15 @@ class RogueCar:
         currentX = self.startX
         currentY = self.startY
 
-        lasttime = starttime
         lasttime = time.time()
 
-        # while math.sqrt((currentX - self.destX)**2 + (currentY - self.destY)**2) > closeness:
         while geodistance(currentX, currentY, self.destX, self.destY) > closeness:
             # pick a random place to go next, that is probably but not definitely closer to the destination
-            mu = (currentX + self.destX) / 2
+            mu = (currentX + self.destX) * 0.5
             sigma = abs(currentX - self.destX) * stupidity
             newX = random.normalvariate(mu, sigma)
 
-            mu = (currentY + self.destY) / 2
+            mu = (currentY + self.destY) * 0.5
             sigma = abs(currentY - self.destY) * stupidity
             newY = random.normalvariate(mu, sigma)
 
@@ -225,50 +220,41 @@ class RogueCar:
         self.waypoints.append(Waypoint(arrival, bestLot.lot.location.latitude, bestLot.lot.location.longitude))
         self.bestLot = bestLot
         attempt = Attempt(arrival, 20, self)
-        logger.info("rogue init")
         self.first_attempt = attempt
         self.tried.append(bestLot)
 
     def get_position(self, now):
-        # logger.info("rogue get position called")
         endTime = 0
         waypointIndex = 0
-
-        if now > self.waypoints[-1].time:
-            self.drawing = False
-            return 1.0, 1.0
 
         while endTime < now:
             waypointIndex += 1
             if waypointIndex > len(self.waypoints) - 1:
                 self.drawing = False
-                return 0, 0
+                return 1.0, 1.0
             endTime = self.waypoints[waypointIndex].time
 
         start = self.waypoints[waypointIndex - 1]
         end = self.waypoints[waypointIndex]
 
         latdiff = end.lat - start.lat
-        # logger.info("lat diff: " + str(latdiff))
         longdiff = end.long - start.long
         timediff = end.time - start.time
         progress = (now - start.time) / timediff
         poslat = start.lat + (latdiff * progress)
         poslong = start.long + (longdiff * progress)
-        # logger.info("before return: " + str(float(poslat)) + ", " + str(float(poslong)))
         return float(poslat), float(poslong)
 
     def park(self):
         pass
 
     async def retry(self, now, oldlot):
-        self.bestLot = None
-        await oldlot.unregister(self)
         self.tried.append(oldlot)
         if len(self.tried) == len(self.manager.lots):
             self.tried = []
         bestDistance = 10000000000000
         # once close enough to the destination, find the nearest parking lot
+        bestLot = None
         for ilot in self.manager.lots:
             currentDistance = geodistance(oldlot.lot.location.latitude, oldlot.lot.location.longitude,
                                           ilot.lot.location.latitude, ilot.lot.location.longitude)
@@ -317,13 +303,6 @@ class Car:
         else:
             return self.lat, self.long
 
-    def set_initial_destination(self, x, y):
-        self.destX = x
-        self.destY = y
-        now = time.time()
-        newtime = now + (self.distance_to(x, y, now) / self.speed)
-        self.waypoints.append(Waypoint(newtime, x, y))
-
     async def set_allocated_destination(self, lot):
         self.aDestX = lot.location.latitude
         self.aDestY = lot.location.longitude
@@ -346,31 +325,30 @@ class Car:
         logger.info("successfully parked user")
         self.drawing = False
 
-    async def retry(self, now, lot):
+    async def retry(self, now, oldlot):
         logger.info("too full for user")
         x, y = self.get_position(now)
 
         self.waypoints = []
         waiting = True
-        lot.unregister(self)
         self.drawing = False
         while waiting and not self.manager.stop_flag:
-            response = await self.cli.send_parking_request(wsmodels.Location(float(x), float(y)), {})
+            await self.cli.send_parking_request(wsmodels.Location(float(x), float(y)), {})
             futs = [self.cli.receive(wsmodels.ParkingAllocationMessage), self.cli.receive(wsmodels.ErrorMessage)]
             (fut,), *_ = await asyncio.wait(futs, return_when=asyncio.FIRST_COMPLETED)
-            space = fut.result()
-            logger.debug('got result: {}'.format(space))
-            if not isinstance(space, wsmodels.ErrorMessage):
+            newlot = fut.result()
+            logger.debug('got result: {}'.format(newlot))
+            if not isinstance(newlot, wsmodels.ErrorMessage):
                 break
             await asyncio.sleep(1)
-        logger.info("successfully reallocated to " + str(space.lot.id))
-        self.waypoints.append(Waypoint(now, lot.lot.location.latitude, lot.lot.location.longitude))
-        distance = geodistance(lot.lot.location.latitude, lot.lot.location.longitude,
-                               space.lot.location.latitude, space.lot.location.longitude)
+        logger.info("successfully reallocated to " + str(newlot.lot.id))
+        self.waypoints.append(Waypoint(now, oldlot.lot.location.latitude, oldlot.lot.location.longitude))
+        distance = geodistance(oldlot.lot.location.latitude, oldlot.lot.location.longitude,
+                               newlot.lot.location.latitude, newlot.lot.location.longitude)
         self.waypoints.append(Waypoint(distance / self.speed,
-                                       space.lot.location.latitude, space.lot.location.longitude))
+                                       newlot.lot.location.latitude, newlot.lot.location.longitude))
         self.drawing = True
-        self.set_allocated_destination(self.manager.lotdict[space.lot.id])
+        self.set_allocated_destination(self.manager.lotdict[newlot.lot.id])
 
 
 class Attempt:
@@ -400,67 +378,13 @@ class ParkingLot:
         self.client = client
 
     async def register(self, attempt: Attempt):
-
-        logger.info("yolo")
         now = time.time()
         asyncio.get_event_loop().create_task(attempt_routine(attempt.arrival - now,
                                                              attempt.car, self, attempt.duration))
 
-        self.attempts.append(attempt)
-
-        self.attempts.sort(key=lambda x: x.arrival)
-
-        # for each car, we need to work out if the lot will be full when it arrives
-        for a in self.attempts:
-            car = a.car
-            self.cars[car] = True
-            # arrival = a.arrival
-            # rival_arrival = 0
-            # rival_index = 0
-            # current_capacity = self.lot.capacity
-            # rivals = []
-            # while rival_arrival < arrival:
-            #     #how many cars arrive before this car
-            #     rivals.append(self.attempts[rival_index])
-            #     rival_arrival = self.attempts[rival_index].arrival
-            #     rival_index += 1
-            #     current_capacity -= 1
-            #
-            # if current_capacity > 0:
-            #     # lot is definitely empty; it never filled up before this car arrived
-            #     self.cars[car] = True
-            # else:
-            #     # spaces could still open up when cars leave
-            #     for rival in rivals:
-            #         departure = rival.arrival + rival.duration
-            #         if departure < arrival:
-            #             current_capacity += 1
-            #
-            #     if current_capacity > 0:
-            #         self.cars[car] = True
-            #     else:
-            #         self.cars[car] = False
-
-    async def attempt_to_park(self, car) -> bool:
-        # if self.cars[car]:
-        #     await self.fill_space()
-        # return self.cars[car]
-        if self.available > 0:
-            self.available -= 1
-            return True
-        else:
-            return False
-
-    async def unregister(self, car):
-        for a in range(len(self.attempts)):
-            if self.attempts[a].car is car:
-                del self.attempts[a]
-
     async def fill_space(self) -> bool:
         if self.available > 0:
-            response = await self.client.update_available(self.lot.id, self.available - 1)
-            if response.error:
-                pass
+            await self.client.update_available(self.lot.id, self.available - 1)
             self.available -= 1
             return True
         else:
@@ -468,6 +392,7 @@ class ParkingLot:
 
     async def free_space(self) -> bool:
         if self.available < self.lot.capacity:
+            await self.client.update_available(self.lot.id, self.available + 1)
             self.available += 1
             return True
         else:
@@ -475,15 +400,10 @@ class ParkingLot:
 
     async def change_price(self, new_price):
         self.lot.price = new_price
-        response = await self.client.update_price(self.lot.id, new_price)
-        # TODO error handling
-        # if response.error:
-        #     pass
+        await self.client.update_price(self.lot.id, new_price)
 
     async def delete(self):
-        response = await self.client.delete_lot(self.lot.id)
-        # if response.error:
-        #     pass
+        await self.client.delete_lot(self.lot.id)
 
     async def change_availability(self, value):
         if value > self.lot.capacity | value < 0:
@@ -492,23 +412,14 @@ class ParkingLot:
         if not(isinstance(value, int)):
             raise TypeError("Availability must be an integer")
 
-        response = await self.client.update_available(self.lot.id, value)
-
-        # if response.error == "200":
-        #     self.available = value
-        #     return True
-        # else:
-        #     return False
+        await self.client.update_available(self.lot.id, value)
 
 
 async def car_routine(startt, start_loc, manager):
     await asyncio.sleep(startt)
 
-
-    logger.info("car routine started")
     car_id = str(uuid4())
     cli = await CarWebsocket.create(base_url=manager.app_url.replace('http', 'ws') + "/ws", user_id=car_id)
-    logger.info("car websocket client connected")
 
     car = Car(start_loc, manager, cli)
     manager.cars.append(car)
@@ -565,25 +476,17 @@ async def space_routine(startt, start_loc, capacity, name, price, available, man
     manager.lotdict[lot.id] = simlot
     manager.lots.append(simlot)
 
-    # await asyncio.sleep(10)
-    #
-    # logger.debug(f'changing price for lot {lot.id}')
-    # await simlot.change_price(1.0)
-    # logger.info(f'Changed price for lot {lot.id} to {1.0}')
-
 
 async def rogue_routine(startt, loc, dest, manager):
     await asyncio.sleep(startt)
-    # rogue = RogueCar(time.time(), loc, dest, manager)
     rogue = await RogueCar.create_rogue(startt, loc, dest, manager)
     rogue.drawing = True
     manager.rogues.append(rogue)
 
 
 async def attempt_routine(delay, car, plot: ParkingLot, duration):
-    logger.info("attempt_routine")
     await asyncio.sleep(delay)
-    success = await plot.attempt_to_park(car)
+    success = await plot.fill_space()
     now = time.time()
     if success:
         car.park()
@@ -595,7 +498,5 @@ async def attempt_routine(delay, car, plot: ParkingLot, duration):
 if __name__ == '__main__':
     sim = SimManager(2000, 20, 70, 50, 50, 1000, 500, 2, 4, 100, "127.0.0.1")
     asyncio.ensure_future(sim.run())
-    #stop simulation after 10 seconds
     asyncio.ensure_future(sim.stop(60))
     asyncio.get_event_loop().run_forever()
-    #can access sim variables here for tests
